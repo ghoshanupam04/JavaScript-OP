f95ab803a17a4e0a84de36f6451bf7064b9690e0
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-06-26 05:55:29 +0000

    done for today

---
+++ 04_DatatypeConversion/datatypeconversion.js
@@ -0,0 +1,28 @@
+/*DataType Conversion:*/
+
+//String to Number
+let name= "Anupam Ghosh"; // Here we can declare that in string.
+console.log(typeof name);
+
+//here er change the type 
+let valueInNumber= Number(name); 
+console.log(typeof valueInNumber ); //we can convart String to Number.
+
+//Number to String
+const age=21;
+console.log(typeof age); //this is a number.
+
+const valueInString=String(age);
+console.log(typeof valueInString); //convart number into a String.
+
+//check if Boolean:
+const phone=false; //true means 1 and false means 0. 
+const valueInNumber= Number(phone);
+console.log(valueInNumber);
+
+//Check complex one 
+const myname= "Anupam04";
+const valueInNumber=Number(myname);
+console.log(valueInNumber); //NAN means Not a Number
+
+
