ba89e6a9d378f248ebf74cef77ffacdcd76d51eb
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-06-26 06:44:02 +0000

    done

---
+++ 05_Operations/operation.js
@@ -0,0 +1,28 @@
+//Check some operations:
+console.log(1+2+3);
+console.log("1"+2+3);
+console.log(1+"2"+3);
+console.log(1+2+"3"%3);
+
+/* If we declare string at the first stage then all are make string and when we declare string at the last then all are declare 
+   number and perform operation and after thet that are performed like a string. */
+
+   //prefix and postfix
+   let x = 3;
+   const y = ++x;
+   
+   console.log(`x:${x}, y:${y}`);
+   // Expected output: "x:4, y:3"
+   
+   let a = 3;
+   const b = ++a;
+   
+   console.log(`a:${a}, b:${b}`);
+   // Expected output: "a:4, b:4"
+
+
+let number =100;
+++number ;
+console.log(number);
+
+   
