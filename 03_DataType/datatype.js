242e33c3129bdac0cc9765eb0395e22cb35e3c47
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-06-26 05:24:58 +0000

    commit chnages

---
+++ 03_DataType/datatype.js
@@ -0,0 +1,15 @@
+//Declare Data Type:
+
+const name="Anupam Ghosh"; //String
+const age=21; //Number
+const college=true; //boolean
+const height= null; //null  /* Null is a Object */
+let phone; //undefined
+const sym= Symbol("foo") // symbol
+
+/* use typeof to show the type */
+console.log(typeof sym);
+
+
+//non primitiv DataType:
+/* Object, Array, Function */
