cb78b662702e32d0648891faa27c4588269aa8ab
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-07-07 07:39:46 +0000

    Describe total Object

--- 13_Object/index.js
+++ 13_Object/index.js
@@ -27,3 +27,62 @@ myObj.greeting=function(){
     console.log(`Hello Codres, I am ${this.FirstName}` );
 }
 console.log(myObj.greeting());
+
+
+//Singelton object:
+const SingletonObject=new Object();
+
+//Non-Singleton Object:
+const non_Singleton_Object={};
+non_Singleton_Object.id="21BT04",
+non_Singleton_Object.name="Batman",
+non_Singleton_Object.gmail="bat69@gmail.com",
+non_Singleton_Object.login=false,
+console.log(non_Singleton_Object);
+
+//Make object in the object /Nested Object
+const nestedObject={
+    FullName:{
+        username:{
+            first_name:"Anupam",
+            last_name:"Ghosh"
+        }
+    }
+}
+console.log(nestedObject.first_name);
+
+
+//Concat / Combine two objects:
+const obj1={
+    UserName:"Anupam_Ghosh",
+    Email:"anupam@gmail.com",
+    id:17301221025
+}
+console.log(Object.keys(obj1)); //Check the keys
+console.log(Object.values(obj1)); //Check the values
+console.log(Object.hasOwnProperty("Email")); //Check the values
+
+
+const onj2={
+    City:"Kolkata",
+    State:"West Bengal"
+}
+const concatObj={...obj1,...onj2}; //spread
+console.log(concatObj);
+
+const concatrwo=Object.assign({},obj1,onj2); //Object.assign
+console.log(concatObj);
+
+//When we fetch some data on the server and need info then the format is :
+const info=[
+    {
+        name:"Anupam Ghosh"
+    },
+    {
+        roll:"21/AB/89"
+    },
+    {
+        city:"Kolkata"
+    }
+]
+console.log(info[2].city); //fetch data 
