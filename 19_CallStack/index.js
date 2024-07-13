8f4c429e49b6bd16cef43ccbe44802f5ed4afb1d
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-07-13 03:27:38 +0000

    CallStack Done

---
+++ 19_CallStack/index.js
@@ -0,0 +1,21 @@
+/* CallStcak: JavaScript uses a Call Stack to track the functions in a program. 
+    The call stack works on the Last In, First Out (LIFO) principle. */
+
+
+//Declare Callstack:
+function one(){
+    console.log('One');
+    two();
+}
+function two(){
+    console.log("two");
+    three();
+}
+function three(){
+    console.log("three");
+    
+}
+
+one();
+two()
+three();
