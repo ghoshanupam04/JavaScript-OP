786378cfa22d62d9b51679b4ac11db3233fbf72d
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-07-16 06:33:14 +0000

    loop donre

--- 21_Loops/index.js
+++ 21_Loops/index.js
@@ -6,5 +6,26 @@ for(i=1;i<=no;i++){ //here we create a condition
     console.log(i); // call the variable 
 }
 
+//Describe gor loops using some condition
+for(let i=0;i<10;i++){
+    let number=i;
+    if(number==5){
+        console.log("Print the number -5");
+    }
+    console.log(i);
+}
 
-//For loop using some condition
+//Nested for loops: / number table
+for(let i=1; i<=10;i++){
+    console.log(`The innerloop value is ${i}`);
+    for(let j=1;j<=10;j++){
+        console.log(i+" * "+j+" = "+ i*j);
+    }
+}
+
+//Print Array element using loops:
+const myArray=["Superman","Flash","Batman","Wondawoman","Aqualaman"];
+for(let i=myArray;i<=myArray.length;i++){
+    const arrel=myArray[i];
+    console.log(arrel);
+}
