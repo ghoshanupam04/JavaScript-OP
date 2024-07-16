e775d38b28baba105e62bc464eaddce12de446f4
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-07-16 06:07:00 +0000

    describe loops

---
+++ 21_Loops/index.js
@@ -0,0 +1,10 @@
+/* Descuss on the Loop Topics */
+
+//for loop:
+const no=10;
+for(i=1;i<=no;i++){ //here we create a condition 
+    console.log(i); // call the variable 
+}
+
+
+//For loop using some condition
