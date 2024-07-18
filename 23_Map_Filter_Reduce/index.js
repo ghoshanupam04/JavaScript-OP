7b2f585052b4d92410bbccaddade20859c62ffcd
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-07-18 07:17:05 +0000

    Discuss about Map, Filter and Reduce

--- 23_Map_Filter_Reduce/index.js
+++ 23_Map_Filter_Reduce/index.js
@@ -28,3 +28,76 @@ MyArr.forEach((value)=>{
 })
 console.log(MySecondArr);
 
+//Make a Exercise to Indepth learn Filter Function:
+const books=[
+    {
+        "Book-Name":"Ikigi",
+        "Price":100,
+        "Country":"Japan"
+    },
+    {
+        "Book-Name":"Hypothesis Love",
+        "Price":120,
+        "Country":"America"
+    },
+    {
+        "Book-Name":"The Hungry Tide",
+        "Price":150,
+        "Country":"India"
+    },
+    {
+        "Book-Name":"Murakami",
+        "Price":120,
+        "Country":"Spanish"
+    },
+    {
+        "Book-Name":"The Last Bridth",
+        "Price":100,
+        "Country":"Spaish"
+    },
+]
+//Check a condition
+const MyBooks=books.filter((poetry)=>(poetry.Price===100)) //here we create condition
+console.log(MyBooks); //print the things wich do you need.
+
+//Check another condition
+const MyBookCountry=books.filter((country)=>(country.Country==="India"))
+console.log(MyBookCountry);
+
+//Map Function:
+const Numbers=[1,2,3,4,5,6,7,8,9];
+const addone=Numbers.map((values)=> values+10); //Add 10 of each numbers
+console.log(addone); //print that 
+
+//Create Chaining Method;
+const numbers=[1,2,3,4,5,6,7,8,9];
+const chainingMethod=numbers
+    .map((value)=>value*100) //After performinng operation pass this.
+    .map((value)=>value+10) // Clain first value and perform operation.
+    .filter((value)=> value>510) // ... 
+console.log(chainingMethod);
+
+//Declare Reduce Function:
+const myNum=[1,2,3];
+const totalSum=myNum.reduce((acc,carVal)=>acc+carVal ,0) //here 0 we can initial the value
+console.log(`The total Value is: ${totalSum}`);
+
+//We use Reduce Function on The Array-object;
+const myDeclareArr=[
+    {
+        "Cource":"JavaScript",
+        "Price":976,
+    },
+    {
+        "Cource":"Java",
+        "Price":2089,
+    },
+    {
+        "Cource":"Python",
+        "Price":999
+    },
+]
+
+const addthePrice=myDeclareArr.reduce((acc,carVal)=>acc+carVal.Price,0)
+console.log(`The Total Price of the Cource's are: ${addthePrice} Rupee`);
+
