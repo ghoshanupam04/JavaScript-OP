f41c2f22b838229c8d4d66eb5eaca30464cc7555
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-08-14 16:24:03 +0000

    Discuss Prototype and Objects

--- 38_Oops/02_Objects/index.js
+++ 38_Oops/02_Objects/index.js
@@ -1,7 +1,7 @@
 //In JavaScript all the things are Object.
 
 //So the big question?? Function is a object?
-
+// => function is a function and this is also a object.
 //describe this in code:
 function multiplication(number){
     return number*5;
@@ -11,8 +11,24 @@ console.log(multiplication(5));
 console.log(multiplication.power);
 console.log(multiplication.prototype); //Empry Object 
 
-// => function is a function and this is also a object.
 
+//Prototype:
+const name="Anupam   ";
+console.log(name.length); //Counting space and o/p given 
+console.log(name.trim().length); //counting no space the exact string
 
+const myHeros=["Spiderman","Deadpool","Wolvorin"]
+const heroPower={
+    Spiderman:"Siling",
+    Deadpool:"Gun",
+    Wolvorin:"Blade",
 
+    SpiderPower:function(){
+        console.log(`The Spiderman Power is: ${this.Spiderman}`);
+    }
+}
+Object.prototype.me=function(){
+    console.log(`I am Present in All Obejcts`);
+}
+heroPower.me()
 
