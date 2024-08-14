1a2d0cb5129760f7631ade8cf5e11bf8e481289c
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-08-14 07:12:25 +0000

    Discuss Functions and this keyword

---
+++ 38_Oops/02_Objects/index.js
@@ -0,0 +1,18 @@
+//In JavaScript all the things are Object.
+
+//So the big question?? Function is a object?
+
+//describe this in code:
+function multiplication(number){
+    return number*5;
+}
+multiplication.power=2
+console.log(multiplication(5));
+console.log(multiplication.power);
+console.log(multiplication.prototype); //Empry Object 
+
+// => function is a function and this is also a object.
+
+
+
+
