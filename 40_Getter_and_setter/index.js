a96ff02b0928f348b72b036e93124351886811d3
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-08-20 15:16:15 +0000

    Discuss Getter and Setter

--- 40_Getter_and_setter/index.js
+++ 40_Getter_and_setter/index.js
@@ -17,4 +17,38 @@ class User{
 const User_Anupam=new User("ghoshanupam@google.com","abc");
 console.log(User_Anupam.password);
 
-//Old Syntax when using getter and setter:
+//Old Syntax, using getter and setter when we not declare soort way:
+
+//Function based:
+function A_User(email,password){
+    this._email=email;
+    this._password=password;
+
+    Object.defineProperty(this,"email",{
+        get: function(){
+            return this._email.toUpperCase();
+        },
+        set: function(value){
+            this._email=value;
+        }
+    })
+}
+const new_user=new A_User("ghoshanupam@google.com");
+console.log(new_user.email);
+
+//Object based:
+const TheUser={
+    "_email":"ghoshanupam@google.com",
+    "_password":"12345098",
+
+    get email(){
+        return this._email.toUpperCase();
+    },
+    set email(value){
+        this._email=value;
+    }
+}
+console.log(TheUser.email);
+
+/*We use Underscore because without underscore they go to the race condition and make stack
+CSSFontFeatureValuesRule, so we use underline.*/
